27b07bf85e8f645c77801d0385465739c94df090
soulcarebymonika <soulcarebymonika@gmail.com>
2026-09-19 19:22:52 +0530

    Fix session authentication key in reviews.php to use auth.php

--- admin/reviews.php
+++ admin/reviews.php
@@ -2,11 +2,7 @@
 // admin/reviews.php
 // Admin page to view, approve, and delete submitted client reviews.
 
-session_start();
-if (!isset($_SESSION['admin_logged_in'])) {
-    header("Location: login.php");
-    exit;
-}
+require_once "auth.php";
 
 require_once "config.php";
 
